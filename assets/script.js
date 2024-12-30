51806b5572678a19e7392dc3b67f47992536d2c5
JamesAChavesForero <javeschames2024@gmail.com>
2024-12-30 13:45:34 -0500

    bugfixes made

--- assets/script.js
+++ assets/script.js
@@ -6,27 +6,56 @@ document.querySelector(".sendBtn").addEventListener("click", (e) => {
   html2pdf(pdfList);
 });
 
+let fromToInfo = {};
+
 Array.from(document.querySelectorAll("input")).forEach((input) => {
   input.addEventListener("focusout", ({ target }) => {
-    console.log(target.value);
-    console.log(target.name);
+    fromToInfo[`${target.name}`] = `${target.value}`;
     document.getElementById(`${target.name}`).innerText = `${target.value}`;
   });
 });
 Array.from(document.querySelectorAll("select")).forEach((input) => {
   input.addEventListener("change", ({ target }) => {
     if (target.name === "route") {
-      document.getElementById(
-        `${target.name}`
+      document.getElementById(`${target.name}`
       ).innerHTML = `<strong>${target.value}</strong>`;
     }
     if (target.name === "Qty") {
-      console.log(target.value)
+      document.getElementById("right").innerHTML = ""
       for (let i = 0; i < target.value; i++) {
-        let clone = document.querySelector(".pdf").cloneNode(true);
-        document.getElementById("right").appendChild(clone);
-      }
-
+        console.log(i , "/",target.value,fromToInfo);
+        document.getElementById("right").innerHTML += 
+        ` <article class="pdf">
+        <span id="custName"> ${fromToInfo.custName} </span>
+        <strong>
+            <span id="invoice"> ${fromToInfo.invoice}</span>
+        </strong>
+        <span id="address"> ${fromToInfo.address} </span>
+        <span id="prodName">${fromToInfo.prodName}</span>
+        <small id="route">${fromToInfo.notes}</small>
+        <small id="qty">${i+1}/${target.value}</small>
+    </article>
+    <div class="html2pdf__page-break"></div>
+    `
+            }
     }
   });
 });
+
+// if (target.name === "Qty") {
+//   console.log(target.value)
+//   for (let i = 0; i < target.value; i++) {
+//     document.getElementById("right") += `
+//     <article class="pdf">
+//             <span id="custName"> Customer Name </span>
+//             <strong>
+//                 <span id="invoice"> Invoice # </span>
+//             </strong>
+//             <span id="address"> Full Address </span>
+//             <span id="prodName">Product Name and Description Specs</span>
+//             <span id="route"></span>
+//             <span id="qty"></span>
+//     </article>`
+//   }
+
+// }
